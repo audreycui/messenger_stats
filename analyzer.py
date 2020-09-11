a86dd365de5ec70078c2bd353071db4562dc0d63
Audrey Cui <audreycui01@audreys-mbp.attlocal.net>
2020-09-11 16:45:39 -0700

    iterate thru all message files

--- analyzer.py
+++ analyzer.py
@@ -14,43 +14,47 @@ def analyze_all():
         print(dir)
         analyze(dir)
 
-def analyze(dir):
-    #start with one file
-    msg_dir = '/Users/audreycui01/dev/messenger_stats/inbox/'+dir +'/message_1.json'
-    reax_count = {"heart": 0, "sad": 0, "laugh": 0, "angry": 0, "wow":0, "like": 0, "dislike": 0}
-    with open(msg_dir) as f:
-        data = json.load(f)
-    ppl_arr = []
-    ppl_dict = {}
-    rev_ppl_dict = {}
-    i =0
+def analyze(dir): #get reaccs stats for convo with one person
+    msg_dir = inbox_dir + dir +'/' #+'/message_1.json'
+    all_msg = listdir(msg_dir)
+
+    ppl_arr = [] #array containing dictionaries of each person's reaccs
+    ppl_dict = {} #dictionary mapping ppl to indicies
+    rev_ppl_dict = {} #reverse dictionary of the above
+
+    with open(msg_dir + "message_1.json") as f:
+        data = json.load(f)  # first file for getting participants
+    i = 0
     for ppl in data["participants"]:
         ppl = ppl["name"]
         print(ppl)
-        ppl_arr.append({"heart": 0, "sad": 0, "laugh": 0, "angry": 0, "wow":0, "like": 0, "dislike": 0, "total_reax":0})
+        ppl_arr.append(
+            {"heart": 0, "sad": 0, "laugh": 0, "angry": 0, "wow": 0, "like": 0, "dislike": 0, "total_reax": 0})
         ppl_dict[ppl] = i
         rev_ppl_dict[i] = ppl
-        i+=1
-    messages = data["messages"]
-    for msg in messages:
-        #actor = msg["reactions"][0]["actor"]
-        try:
-            actor = (msg["reactions"][0])["actor"]
-            actor_index = ppl_dict[actor]
-            reax = msg["reactions"][0]["reaction"]
-            reax = reax_conversion[reax]
-            ppl_arr[actor_index][reax] += 1
-            ppl_arr[actor_index]["total_reax"] += 1
-        except KeyError:
-            x=0 #do nothing lol
-    i=0
+        i += 1
 
+    for msg_file in all_msg: #iterate thru all message files to count reaccs
+        if msg_file.find("message") >= 0 and msg_file.find("message_1") <0:
+
+            messages = data["messages"]
+            for msg in messages:
+                #actor = msg["reactions"][0]["actor"]
+                try:
+                    actor = (msg["reactions"][0])["actor"]
+                    actor_index = ppl_dict[actor]
+                    reax = msg["reactions"][0]["reaction"]
+                    reax = reax_conversion[reax]
+                    ppl_arr[actor_index][reax] += 1
+                    ppl_arr[actor_index]["total_reax"] += 1
+                except KeyError:
+                    x=0 #do nothing lol
+    i=0
     #raw numbers
     for a in ppl_arr:
         print(rev_ppl_dict[i])
         print(a)
         i+=1
-
 
     i=0
     #frequency
@@ -69,7 +73,7 @@ def analyze(dir):
 
 #analyze_all()
 
-analyze("AllenLi_DTkZUbw4eQ") 
+analyze("insert directory name for chat here :)")
 
 
 
